e77e7dfb92c534381c5296e577e14d7be7e9eacc
alberto-cardini <albycardini@gmail.com>
2024-07-10 23:01:25 +0200

    Variables' name refactor

--- inc/Matrix.hpp
+++ inc/Matrix.hpp
@@ -6,26 +6,26 @@
 #define MINIPHOTOEDITOR_MATRIX_HPP
 #include <iostream>
 #include <vector>
-#include "Pixel.hpp"
 
-template <typename T>
+template <typename T, typename N = double>
 class Matrix {
 public:
     Matrix(int h, int w) : height(h), width(w) {
         matrix.reserve(height * width);
     }
 
-    int  getHeight() const { return height; }
-    int  getWidth() const { return width; }
+    int   getHeight() const { return height; }
+    int   getWidth() const { return width; }
+    auto& getMatrix() { return matrix; };
 
-    void insertValue(const std::vector<T>& valuesArray) {
+    void  insert(const std::vector<T>& valuesArray) {
         if (valuesArray.size() == height * width)
             for (int i = 0; i < height * width; ++i) {
                 matrix.push_back(valuesArray[i]);
             }
     }
 
-    void insertValue(const T& value) {
+    void insert(const T& value) {
         try {
             matrix.push_back(value);
         } catch (const std::runtime_error& e) {
@@ -33,8 +33,8 @@ public:
         }
     }
 
-    Matrix<T>& operator=(const Matrix<T>& right) {
-        if (this == *right) {
+    Matrix& operator=(const Matrix<T>& right) {
+        if (this != &right) {
             height = right.height;
             width  = right.width;
             matrix = right.matrix;
@@ -74,51 +74,88 @@ public:
         return result;
     }
 
-    T average() {
+    T arithmeticAvg() {
         T temp = 0;
         for (int i = 0; i < height * width; ++i) { temp = (temp + matrix[i]); }
         temp = temp / (height * width);
         return temp;
     }
 
-    T convolve(const Matrix<T>& right) {
-        if (right.height % 2 == 1 && right.width % 2 == 1) {
-            reallocate(right.height - (right.height / 2), right.average());
-
-            // TODO: Implement the convolution
+    void convolve(Matrix<N>& h) {
+        if (h.getHeight() % 2 == 1 && h.getWidth() % 2 == 1) {
+            int       plus = h.getWidth() - (h.getWidth() / 2 + 1);
+            Matrix<T> convolved_M(height, width);
+            reallocate(plus, arithmeticAvg());
+            h.flipAxisY();
+            h.flipAxisX();
+            T temp = 0;
+            for (int i = 0; i < height - (plus * 2); ++i) {
+                for (int j = 0; j < width - (plus * 2); ++j) {
+                    for (int m = 0; m < h.getHeight(); ++m) {
+                        for (int n = 0; n < h.getWidth(); ++n) {
+                            temp =
+                                temp + matrix[height * (i + m) + j + n] *
+                                           h.getMatrix()[h.getHeight() * m + n];
+                        }
+                    }
+                    convolved_M.matrix.push_back(temp);
+                    temp = 0;
+                }
+            }
+            *this = convolved_M;
         }
     }
 
-    // Works only with square matrix.
+    void flipAxisX() {
+        Matrix<T> flippedMatrix(height, width);
+        for (int i = height - 1; i >= 0; --i) {
+            for (int j = 0; j < height; ++j) {
+                flippedMatrix.matrix.push_back(matrix[height * i + j]);
+            }
+        }
+        *this = flippedMatrix;
+    }
+
+    void flipAxisY() {
+        Matrix<T> flippedMatrix(height, width);
+        for (int i = 0; i < height; ++i) {
+            for (int j = width - 1; j >= 0; --j) {
+                flippedMatrix.matrix.push_back(matrix[height * i + j]);
+            }
+        }
+        *this = flippedMatrix;
+    }
+
+    // Works only with square kernel.
     void reallocate(int plus, T filler) {
         int newHeight           = height + plus * 2;
-        // add the plus * 2 extra lines to the matrix. This is needed to create
+        // add the plus * 2 extra lines to the kernel. This is needed to create
         // the extra space for the convolution of the first element, otherwise
         // the index of the h(x - tau) would go out of range
         int            newWidth = width + plus * 2;  // same with the columns
 
-        std::vector<T> newMatrix;
-        newMatrix.reserve(newHeight * newWidth);
+        Matrix<T>      newMatrix(newHeight, newWidth);
+        std::vector<T> filledVector;
+        filledVector.reserve(newHeight * newWidth);
 
         for (int i = 0; i < newHeight * newWidth; ++i) {
-            newMatrix.push_back(filler);
+            filledVector.push_back(filler);
         }
 
         for (int i = plus; i < newHeight - plus; ++i) {
             for (int j = plus; j < height + plus; ++j) {
-                newMatrix[newHeight * i + j] =
+                filledVector[newHeight * i + j] =
                     matrix[height * (i - plus) + (j - plus)];
             }
         }
-
-        matrix = newMatrix;
-        height = newHeight;
-        width  = newWidth;
+        newMatrix.insert(filledVector);
+        *this = newMatrix;
     };
 
     T& operator[](int position) { return matrix.at(position); }
 
 private:
+
     std::vector<T> matrix;
     int            height;
     int            width;
