db55119a26ada2f5c45689e02437286e74eed05d
alberto-cardini <albycardini@gmail.com>
2024-07-12 21:54:59 +0200

    Abstract class to unify filters' interface

---
+++ inc/Filter.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Alberto Cardini on 29/06/24.
+//
+
+#ifndef MINIPHOTOEDITOR_FILTER_HPP
+#define MINIPHOTOEDITOR_FILTER_HPP
+#include <iostream>
+#include <memory>
+#include <cmath>
+#include "Image.hpp"
+
+class Filter {
+public:
+    virtual ~Filter() = default;
+    virtual void apply(Image& img) = 0;
+};
+
+#endif  // MINIPHOTOEDITOR_FILTER_HPP
