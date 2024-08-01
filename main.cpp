9b64620f799c3797d0ee6fc65f710fa854b45217
alberto-cardini <albycardini@gmail.com>
2024-08-01 17:47:56 +0200

    Setup of the Test Suite

--- main.cpp
+++ main.cpp
@@ -2,10 +2,40 @@
 // Created by Alberto Cardini on 20/05/24.
 //
 #include <iostream>
+#include "EdgeDetection.hpp"
 #include "Filter.hpp"
-#include "Image.hpp"
+#include "RGB_image.hpp"
+#include "Gray_Scale_image.hpp"
 
 int main() {
+
+    auto startLoad = std::chrono::system_clock::now();
+
+    RGB_image i1("input/sunflower.ppm", "output/prova4.ppm");
+    Gray_Scale_image i2("input/sunflower_gray_scale.pgm", "output/Edge7.pgm");
+
+    auto endLoad = std::chrono::system_clock::now();
+    float elapsedLoad = std::chrono::duration_cast<std::chrono::seconds>(endLoad-startLoad).count();
+    printf("Time %.4fms\n", elapsedLoad);
+
+    auto start = std::chrono::system_clock::now();
+
+    EdgeDetection filter(30,60);
+    filter.apply(i2);
+
+    auto end = std::chrono::system_clock::now();
+    float elapsed = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
+    printf("Time %.4fms\n", elapsed);
+
+
+    auto startSave = std::chrono::system_clock::now();
+
+    i2.save();
+    i1.save();
+
+    auto endSave = std::chrono::system_clock::now();
+    float elapsedSave = std::chrono::duration_cast<std::chrono::seconds>(endSave-startSave).count();
+    printf("Time %.4fms\n", elapsedSave);
 
     return 0;
 }
---
+++ test/RGB_Image_Test.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Alberto Cardini on 25/07/24.
+//
+#include "gtest/gtest.h"
