0f4d326637d54fa6c84e06422f79116f1d2b8e8b
alberto-cardini <albycardini@gmail.com>
2024-09-03 12:44:01 +0200

    Example of usage

--- main.cpp
+++ main.cpp
@@ -2,40 +2,23 @@
 // Created by Alberto Cardini on 20/05/24.
 //
 #include <iostream>
-#include "EdgeDetection.hpp"
-#include "Filter.hpp"
-#include "RGB_image.hpp"
-#include "Gray_Scale_image.hpp"
+
+#include "Edge_Detection.hpp"
+#include "Image.hpp"
 
 int main() {
 
-    auto startLoad = std::chrono::system_clock::now();
+    Image i1("test_asset/sunflower.ppm");
+    Edge_Detection filter(40,20);
 
-    RGB_image i1("input/sunflower.ppm", "output/prova4.ppm");
-    Gray_Scale_image i2("input/sunflower_gray_scale.pgm", "output/Edge7.pgm");
+    Image i2("test_asset/sunflower.ppm");
+    Gaussian_Blur blur_filter(7);
 
-    auto endLoad = std::chrono::system_clock::now();
-    float elapsedLoad = std::chrono::duration_cast<std::chrono::seconds>(endLoad-startLoad).count();
-    printf("Time %.4fms\n", elapsedLoad);
+    filter.apply(i1);
+    i1.save("output/sunflower_only_edges.ppm");
 
-    auto start = std::chrono::system_clock::now();
-
-    EdgeDetection filter(30,60);
-    filter.apply(i2);
-
-    auto end = std::chrono::system_clock::now();
-    float elapsed = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
-    printf("Time %.4fms\n", elapsed);
-
-
-    auto startSave = std::chrono::system_clock::now();
-
-    i2.save();
-    i1.save();
-
-    auto endSave = std::chrono::system_clock::now();
-    float elapsedSave = std::chrono::duration_cast<std::chrono::seconds>(endSave-startSave).count();
-    printf("Time %.4fms\n", elapsedSave);
+    blur_filter.apply(i2);
+    i2.save("output/sunflower_blurred.ppm");
 
     return 0;
 }
